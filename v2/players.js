62cf8b25eb6d971bd9d5bdb115775a5f7a402427
enesfaruktkccc <rezzakbilir2@gmail.com>
2026-08-18 22:06:32 +0300

    Add 800-player curated quiz pool

---
+++ v2/players.js
@@ -0,0 +1,3 @@
+// 800-player curated pool. Run structure uses 40 very easy / 40 easy / 50 medium / 15 hard / 15 very hard.
+// The wider pool prevents repeated sequences between games.
+"+json.dumps(players, ensure_ascii=False, indent=2)+";
